4ae3ca9955068f5a995d9ade154084d0eab3f813
Sam Wagner <sam.wagner@hey.com>
2026-01-01 21:34:32 +1000

    Initial Commit

---
+++ Phase/PhaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhaseApp.swift
+//  Phase
+//
+//  Created by Sam Wagner on 1/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct PhaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
